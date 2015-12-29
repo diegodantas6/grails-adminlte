7e60559aff470d686c02d6e3f31633a98dd09605
Dantas <Dantas@Dantas-PC>
2015-12-29 18:51:46 -0200

    edit Cliente domain

--- grails-adminlte/grails-app/domain/br/com/teste/Cliente.groovy
+++ grails-adminlte/grails-app/domain/br/com/teste/Cliente.groovy
@@ -6,7 +6,7 @@ class Cliente {
 	String cpf
 
 	static constraints = {
-		nome blank: false, nullable: false
-		cpf blank: false, nullable: false, unique: true
+		nome blank: false, nullable: false, size: 20..60
+		cpf blank: false, nullable: false, unique: true, minSize: 14
 	}
 }
